ebe556c1fa866298bf963c4161809b7bcfeacf42
Troy Mullaney <troym9731@gmail.com>
2017-08-20 11:18:30 -0700

    Setup the script to use numbers read from Postgres

--- src/main.py
+++ src/main.py
@@ -1,18 +1,28 @@
 import os
+import psycopg2
 import requests
 import time
 from bs4 import BeautifulSoup
-from constants import SCOREBOARD_URL, SIGNIN_URL
+from constants import LOWEST_SCORE_MESSAGE, SCOREBOARD_URL, SIGNIN_URL
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from twilio.rest import Client
+from urllib.parse import urlparse
 
 client = Client(os.environ['TWILIO_ACCOUNT'], os.environ['TWILIO_TOKEN'])
+url = urlparse(os.environ['DATABASE_URL'])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
-def scrape(league, year):
-    # Fire up Chrome and go to ESPN signin page
+def initiate_shame(league, year):
+    # Fire up Chrome and go to ESPN's signin page
     driver = webdriver.Chrome()
     driver.get(SIGNIN_URL)
 
@@ -25,7 +35,7 @@ def scrape(league, year):
     time.sleep(2)
 
     # Fill in the form and submit it
-    driver.find_element_by_xpath('(//input)[1]').send_keys(os.environ['USERNAME'])
+    driver.find_element_by_xpath('(//input)[1]').send_keys(os.environ['ESPN_USERNAME'])
     driver.find_element_by_xpath('(//input)[2]').send_keys(os.environ['PASSWORD'])
     driver.find_element_by_class_name('btn-submit').click()
     driver.switch_to_default_content()
@@ -40,24 +50,42 @@ def scrape(league, year):
     # Close Chrome
     driver.quit()
 
+    # Get the Scoreboard URL page
     page = session.get(SCOREBOARD_URL.format(league=league, year=year))
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Select the matchup rows
     score_rows = soup.select('.matchup tr')
+
+    # Create the scoring hash, e.g. {'name': 'John', 'score': 40}
+    # Then, filter out any empty hashes
     scores = list(filter(lambda x: bool(x), map(score_hash, score_rows)))
+
+    # Sort the scores from lowest to highest and grab the lowest
     sorted_scores = sorted(scores, key=lambda x: x['score'])
     lowest_scorer = sorted_scores[0]
-    body = 'The bottom feeder of the week is {name} with {score} points'
 
-    message = client.messages.create(
-        to='',
-        from_='',
-        media_url='https://media.giphy.com/media/vX9WcCiWwUF7G/giphy.gif',
-        body=body.format(
-            name=lowest_scorer['name'],
-            score=lowest_scorer['score']
+    # Grab all the phone numbers from the DB
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM phone_numbers')
+    records = cursor.fetchall()
+
+    # Map over the results and just grab the phone number strings
+    phone_numbers = list(map(lambda x: x[0], records))
+
+    # Send a message to all of the phone numbers with the GoT "Shame" gif
+    for number in phone_numbers:
+        message = client.messages.create(
+            to=number,
+            from_=os.environ['TWILIO_PHONE_NUMBER'],
+            media_url='https://media.giphy.com/media/vX9WcCiWwUF7G/giphy.gif',
+            body=LOWEST_SCORE_MESSAGE.format(
+                name=lowest_scorer['name'],
+                score=lowest_scorer['score']
+            )
         )
-    )
 
+# If there isn't an "owner" in the row, then return an empty hash
 def score_hash(row):
     if not bool(row.find(class_='owners')): return {}
     return {
